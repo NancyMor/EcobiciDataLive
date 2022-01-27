68aab8704112de50346ace8c0358c673b7a4ff1d
unknown <nanmorla@gmail.com>
2022-01-26 18:15:01 -0600

    Merge changes

--- R/Analysis.R
+++ R/Analysis.R
@@ -1,9 +1,12 @@
 
+input_path = 'C:/Users/Nan/Documents/EcobiciDataLive/Input'
 output_path = 'C:/Users/Nan/Documents/EcobiciDataLive/Output'
 
-RunEcobici <- function(path_file = "./extdata/dataEcobici/"){
-  Ecobici <- ReadFiles(path_file = "./extdata/dataEcobici/")
+RunEcobici <- function(input_path = NULL, output_path = NULL){
+  Ecobici <- ReadFiles(input_path)
   Ecobici <- CleanFormat(Ecobici)
   Ecobici <- TransformData(Ecobici)
   Ecobici <- ValidateInput(Ecobici)
+  CreateHTML(Ecobici, output_path)
+  createCSV(Ecobici, output_path)
 }
--- R/CreateCSV.R
+++ R/CreateCSV.R
@@ -1,4 +1,4 @@
-createCSVReport <- function(Ecobici, output_path){
+createCSV <- function(Ecobici, output_path){
   ecodata <- Ecobici$ecodata
   output_file <- paste0(output_path, "/ecobiciData.csv")
   write.csv(ecodata, file = output_file)
